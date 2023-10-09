7f32addb5fdf7691e4dbc7b2c065d5c6c6b0a31a
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-09 17:05:50 +0100

    view updated

--- things/migrations/0001_initial.py
+++ things/migrations/0001_initial.py
@@ -1,6 +1,12 @@
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+    
     operations = [
         migrations.CreateModel(
             name='things',
@@ -12,3 +18,5 @@ class Migration(migrations.Migration):
             
         ),
     ]
+
+    
