c4d8aee79a8ba7ed4456b5160cf7cae8a35e911e
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-09 16:41:33 +0100

    view updated

--- things/models.py
+++ things/models.py
@@ -3,10 +3,6 @@ from django.db import models
 
 class Test(models.Model):
     quantity = models.IntegerField(max_length=100)
-    name = models.CharField(
-        unique=True,
-        blank=False,
-        max_length=30
-    )
+    name = models.CharField(unique=True, blank=False, max_length=30)
     description = models.CharField(blank=True, max_length=120)
     
