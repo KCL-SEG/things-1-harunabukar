5cabc84ffd782c01f1cbc9c528b18633b6324d58
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-09 16:35:05 +0100

    view updated

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    quantity = models.IntegerField(max_length=100)
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30
+    )
+    description = models.CharField(blank=True, max_length=120)
+    
