2aa7e2be0324e15ee94dcc6c9f6e5829d1bbce3d
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-09 17:09:05 +0100

    view updated

--- things/migrations/0001_initial.py
+++ things/migrations/0001_initial.py
@@ -6,14 +6,15 @@ class Migration(migrations.Migration):
 
     dependencies = [
     ]
-    
+
     operations = [
         migrations.CreateModel(
-            name='things',
+            name='Thing',
             fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
                 ('quantity', models.IntegerField()),
-                ('description', models.CharField()) 
+                ('description', models.CharField()),
             ],
             
         ),
