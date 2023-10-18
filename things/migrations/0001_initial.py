309da5d59d620e1ccf08b43e1fb50216d52d4445
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-18 10:59:50 +0100

    no changes

--- things/migrations/0001_initial.py
+++ things/migrations/0001_initial.py
@@ -1,4 +1,5 @@
 from django.db import migrations, models
+import django.core.validators
 
 class Migration(migrations.Migration):
 
@@ -13,8 +14,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
-                ('quantity', models.IntegerField()),
-                ('description', models.CharField()),
+                ('quantity', models.IntegerField(validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(100)])),
+                ('description', models.CharField(blank=True, max_length=120, unique=False)),
             ],
         ),
     ]
